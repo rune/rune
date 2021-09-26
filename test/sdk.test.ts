376c9ad866aec8c9a2252be5c6d7877236ddc194
Bjarke <bjarke@felbo.dk>
2021-09-26 11:40:55 -0700

    Tests with Rune SDK reimported each time

---
+++ test/sdk.test.ts
@@ -0,0 +1,44 @@
+import { Rune } from "./setup"
+import { extractErrMsg } from "./helper"
+
+describe("sdk", function () {
+  it("init() -> pauseGame() -> gameOver() -> startGame()", async function () {
+    // Mock a game's state
+    let gameState: "OFF" | "RUNNING" | "PAUSED" = "OFF"
+    Rune.init({
+      startGame: () => {
+        gameState = "RUNNING"
+      },
+      pauseGame: () => {
+        gameState = "PAUSED"
+      },
+      resumeGame: () => {
+        gameState = "RUNNING"
+      },
+    })
+
+    // Should be no change in gameState from calling init()
+    expect(gameState).toMatchInlineSnapshot(`"OFF"`)
+
+    // Should start the game
+    Rune.startGame()
+    expect(gameState).toMatchInlineSnapshot(`"RUNNING"`)
+
+    // Should pause the game
+    Rune.pauseGame()
+    expect(gameState).toMatchInlineSnapshot(`"PAUSED"`)
+
+    // Should restart the game a bit after providing score
+    Rune.gameOver({ score: 0 })
+    expect(gameState).toMatchInlineSnapshot(`"PAUSED"`)
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    expect(gameState).toMatchInlineSnapshot(`"RUNNING"`)
+  })
+
+  it("don't allow calling other functions before init()", async function () {
+    const errMsg = await extractErrMsg(() => {
+      Rune.startGame()
+    })
+    expect(errMsg).toMatchInlineSnapshot(`"Rune.startGame() called before Rune.init()"`)
+  })
+})

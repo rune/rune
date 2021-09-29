9578b03d6f67e0d4f64607bd7b262b81ebbea50b
Bjarke <bjarke@felbo.dk>
2021-09-29 11:04:03 -0700

    Only overwrite process.env if undefined

--- dist/browser.js
+++ dist/browser.js
@@ -2,7 +2,8 @@
 Pure JS code prepended to the exported JS SDK for the browser.
 This is easier and more controllable than getting browserify to work.
 */
-process = { env: {} }
+if (typeof process === "undefined") process = {}
+if (typeof process.env === "undefined") process.env = {}
 /*
 The SDK interface for games to interact with Rune.
 */
--- src/browser.js
+++ src/browser.js
@@ -2,4 +2,5 @@
 Pure JS code prepended to the exported JS SDK for the browser.
 This is easier and more controllable than getting browserify to work.
 */
-process = { env: {} }
+if (typeof process === "undefined") process = {}
+if (typeof process.env === "undefined") process.env = {}

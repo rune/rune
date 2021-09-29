6af7bed789b6403a43ecb0dbfe0110a92a5091ca
Bjarke <bjarke@felbo.dk>
2021-09-28 22:03:11 -0700

    Prepend code to browser export

--- dist/browser.js
+++ dist/browser.js
@@ -1,4 +1,9 @@
 /*
+Pure JS code prepended to the exported JS SDK for the browser.
+This is easier and more controllable than getting browserify to work.
+*/
+process = { env: {} }
+/*
 The SDK interface for games to interact with Rune.
 */
 const Rune = {
--- scripts/build.sh
+++ scripts/build.sh
@@ -6,11 +6,13 @@ rm -rf ./dist/**
 # Compile TS to JS for npm imports
 npx tsc --declaration
 
-# Compile TS to JS for browser imports.
-# Removes "export" to make a version of the SDK that can be used in the browser.
-# This approach is a bit hacky and could be replaced by browserify in the future.
+# Export the SDK to be used in the browser:
+# 1) Compile TS to JS
+# 2) Add a bit of prepended JS code to e.g. setup process.env
+# 3) Add the Rune SDK w/o "export" to add Rune to global namespace
+# This is easier and more controllable than getting browserify to work.
 mkdir dist/browser
 npx tsc --project tsconfig.browser.json
-sed -i '' 's/\export //g' dist/browser/index.js
-mv dist/browser/index.js dist/browser.js
+cat src/browser.js >> dist/browser.js
+sed 's/\export //g' dist/browser/index.js >> dist/browser.js
 rm -rf dist/browser

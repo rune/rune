c455e318734020a1140ca2cd89b777772c9af0c1
Bjarke <bjarke@felbo.dk>
2021-09-28 20:56:53 -0700

    Export browser version

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# Remove existing files from the build folder
-rm -rf ./build/*
+# Remove existing files from the output folder
+rm -rf ./dist/**
 
-# Compile TS to JS
-npx tsc
+# Compile TS to JS for npm imports
+npx tsc --declaration
+
+# Compile TS to JS for browser imports.
+# Removes "export" to make a version of the SDK that can be used in the browser.
+# This approach is a bit hacky and could be replaced by browserify in the future.
+mkdir dist/browser
+npx tsc --project tsconfig.browser.json
+sed -i '' 's/\export //g' dist/browser/index.js
+mv dist/browser/index.js dist/browser.js
+rm -rf dist/browser

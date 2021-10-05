d97f2dca2c8db3a0f71c90f15af04f154b0ee812
Bjarke <bjarke@felbo.dk>
2021-10-05 12:33:32 -0700

    feat: attach browser version to window

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,10 +9,10 @@ npx tsc --declaration
 # Export the SDK to be used in the browser:
 # 1) Compile TS to JS
 # 2) Add a bit of prepended JS code to e.g. setup process.env
-# 3) Add the Rune SDK w/o "export" to add Rune to global namespace
+# 3) Add the Rune SDK using "window" instead of "export" to add Rune to global namespace
 # This is easier and more controllable than getting browserify to work.
 mkdir dist/browser
 npx tsc --project tsconfig.browser.json
 cat src/browser.js >> dist/browser.js
-sed 's/\export //g' dist/browser/index.js >> dist/browser.js
+sed 's/\export const Rune/window["Rune"]/g' dist/browser/index.js >> dist/browser.js
 rm -rf dist/browser

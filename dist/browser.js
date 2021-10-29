c310487decb228ca4e99251266983d666a4a418a
Bjarke <bjarke@felbo.dk>
2021-10-28 17:50:30 -0700

    New dist

--- dist/browser.js
+++ dist/browser.js
@@ -1,17 +1,17 @@
 /*
 The SDK interface for games to interact with Rune.
 */
-window["Rune"] = {
+var Rune = {
     // External properties and functions
-    version: "1.2.0",
-    init: (input) => {
+    version: "1.2.2",
+    init: function (input) {
         // Check that this function has not already been called
         if (Rune._doneInit) {
             throw new Error("Rune.init() should only be called once");
         }
         Rune._doneInit = true;
         // Check that game provided correct input to SDK
-        const { startGame, resumeGame, pauseGame } = input || {};
+        var _a = input || {}, startGame = _a.startGame, resumeGame = _a.resumeGame, pauseGame = _a.pauseGame;
         if (typeof startGame !== "function") {
             throw new Error("Invalid startGame function provided to Rune.init()");
         }
@@ -26,53 +26,57 @@ window["Rune"] = {
         Rune._resumeGame = resumeGame;
         Rune._pauseGame = pauseGame;
         // When running inside Rune, runePostMessage will always be defined.
-        if (globalThis.postRuneEvent) {
-            globalThis.postRuneEvent({ type: "INIT", version: Rune.version });
+        if (window.postRuneEvent) {
+            window.postRuneEvent({ type: "INIT", version: Rune.version });
         }
         else {
             mockEvents();
         }
     },
-    gameOver: ({ score }) => {
-        var _a;
+    gameOver: function (_a) {
+        var _b;
+        var score = _a.score;
         if (!Rune._doneInit) {
             throw new Error("Rune.gameOver() called before Rune.init()");
         }
         if (typeof score !== "number") {
             throw new Error("Score provided to Rune.gameOver() must be a number");
         }
-        (_a = globalThis.postRuneEvent) === null || _a === void 0 ? void 0 : _a.call(globalThis, { type: "GAME_OVER", score });
+        (_b = window.postRuneEvent) === null || _b === void 0 ? void 0 : _b.call(window, { type: "GAME_OVER", score: score });
     },
     // Internal properties and functions used by the Rune app
     _doneInit: false,
-    _startGame: () => {
+    _startGame: function () {
         throw new Error("Rune._startGame() called before Rune.init()");
     },
-    _resumeGame: () => {
+    _resumeGame: function () {
         throw new Error("Rune._resumeGame() called before Rune.init()");
     },
-    _pauseGame: () => {
+    _pauseGame: function () {
         throw new Error("Rune._pauseGame() called before Rune.init()");
-    },
+    }
 };
 // Create mock events to support development
-const mockEvents = () => {
+var mockEvents = function () {
     // Log posted events to the console (in production, these are processed by Rune)
-    globalThis.postRuneEvent = (event) => console.log(`RUNE: Posted ${JSON.stringify(event)}`);
+    window.postRuneEvent = function (event) {
+        return console.log("RUNE: Posted " + JSON.stringify(event));
+    };
     // Mimic the user tapping Play after 3 seconds
-    console.log(`RUNE: Starting new game in 3 seconds.`);
-    setTimeout(() => {
+    console.log("RUNE: Starting new game in 3 seconds.");
+    setTimeout(function () {
         Rune._startGame();
-        console.log(`RUNE: Started new game.`);
+        console.log("RUNE: Started new game.");
     }, 3000);
     // Automatically restart game 3 seconds after Game Over
-    Rune.gameOver = function ({ score }) {
-        var _a;
-        (_a = globalThis.postRuneEvent) === null || _a === void 0 ? void 0 : _a.call(globalThis, { type: "GAME_OVER", score });
-        console.log(`RUNE: Starting new game in 3 seconds.`);
-        setTimeout(() => {
+    Rune.gameOver = function (_a) {
+        var _b;
+        var score = _a.score;
+        (_b = window.postRuneEvent) === null || _b === void 0 ? void 0 : _b.call(window, { type: "GAME_OVER", score: score });
+        console.log("RUNE: Starting new game in 3 seconds.");
+        setTimeout(function () {
             Rune._startGame();
-            console.log(`RUNE: Started new game.`);
+            console.log("RUNE: Started new game.");
         }, 3000);
     };
 };
--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,19 @@
 /*
 The SDK interface for games to interact with Rune.
 */
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 exports.Rune = void 0;
 exports.Rune = {
     // External properties and functions
-    version: "1.2.0",
-    init: (input) => {
+    version: "1.2.2",
+    init: function (input) {
         // Check that this function has not already been called
         if (exports.Rune._doneInit) {
             throw new Error("Rune.init() should only be called once");
         }
         exports.Rune._doneInit = true;
         // Check that game provided correct input to SDK
-        const { startGame, resumeGame, pauseGame } = input || {};
+        var _a = input || {}, startGame = _a.startGame, resumeGame = _a.resumeGame, pauseGame = _a.pauseGame;
         if (typeof startGame !== "function") {
             throw new Error("Invalid startGame function provided to Rune.init()");
         }
@@ -36,46 +36,50 @@ exports.Rune = {
             mockEvents();
         }
     },
-    gameOver: ({ score }) => {
-        var _a;
+    gameOver: function (_a) {
+        var _b;
+        var score = _a.score;
         if (!exports.Rune._doneInit) {
             throw new Error("Rune.gameOver() called before Rune.init()");
         }
         if (typeof score !== "number") {
             throw new Error("Score provided to Rune.gameOver() must be a number");
         }
-        (_a = globalThis.postRuneEvent) === null || _a === void 0 ? void 0 : _a.call(globalThis, { type: "GAME_OVER", score });
+        (_b = globalThis.postRuneEvent) === null || _b === void 0 ? void 0 : _b.call(globalThis, { type: "GAME_OVER", score: score });
     },
     // Internal properties and functions used by the Rune app
     _doneInit: false,
-    _startGame: () => {
+    _startGame: function () {
         throw new Error("Rune._startGame() called before Rune.init()");
     },
-    _resumeGame: () => {
+    _resumeGame: function () {
         throw new Error("Rune._resumeGame() called before Rune.init()");
     },
-    _pauseGame: () => {
+    _pauseGame: function () {
         throw new Error("Rune._pauseGame() called before Rune.init()");
-    },
+    }
 };
 // Create mock events to support development
-const mockEvents = () => {
+var mockEvents = function () {
     // Log posted events to the console (in production, these are processed by Rune)
-    globalThis.postRuneEvent = (event) => console.log(`RUNE: Posted ${JSON.stringify(event)}`);
+    globalThis.postRuneEvent = function (event) {
+        return console.log("RUNE: Posted " + JSON.stringify(event));
+    };
     // Mimic the user tapping Play after 3 seconds
-    console.log(`RUNE: Starting new game in 3 seconds.`);
-    setTimeout(() => {
+    console.log("RUNE: Starting new game in 3 seconds.");
+    setTimeout(function () {
         exports.Rune._startGame();
-        console.log(`RUNE: Started new game.`);
+        console.log("RUNE: Started new game.");
     }, 3000);
     // Automatically restart game 3 seconds after Game Over
-    exports.Rune.gameOver = function ({ score }) {
-        var _a;
-        (_a = globalThis.postRuneEvent) === null || _a === void 0 ? void 0 : _a.call(globalThis, { type: "GAME_OVER", score });
-        console.log(`RUNE: Starting new game in 3 seconds.`);
-        setTimeout(() => {
+    exports.Rune.gameOver = function (_a) {
+        var _b;
+        var score = _a.score;
+        (_b = globalThis.postRuneEvent) === null || _b === void 0 ? void 0 : _b.call(globalThis, { type: "GAME_OVER", score: score });
+        console.log("RUNE: Starting new game in 3 seconds.");
+        setTimeout(function () {
             exports.Rune._startGame();
-            console.log(`RUNE: Started new game.`);
+            console.log("RUNE: Started new game.");
         }, 3000);
     };
 };

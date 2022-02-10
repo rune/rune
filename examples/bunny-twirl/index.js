75d27bf8f857bf99a576dc5fd7abcb06909a0d51
mieszko4 <mieszko4@gmail.com>
2022-02-10 01:14:32 +0100

    Explain more how randomization fixed list is applied

--- examples/bunny-twirl/index.js
+++ examples/bunny-twirl/index.js
@@ -61,11 +61,13 @@ const challengeNumber = Rune.getChallengeNumber();
 // const nextChallengeNumber = challengeNumber + 1;
 
 // 1. Game map randomization
+// Derive rotation speed (floating-point number between 0.005 and 0.03) from Rune challenge number
 const random = new Math.seedrandom(challengeNumber)
 const pseudoRandomNumber = random()
 const speed = (pseudoRandomNumber / 100) * 3 + 0.005
 
 // 2. Fixed list of game maps
+// Derive direction (-1 or 1) from Rune challenge number
 const directions = [-1, 1]
 const direction = directions[(challengeNumber - 1) % directions.length]
 

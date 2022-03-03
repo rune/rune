83ff8fa0e7aa239449af23216d85850121fae48d
Bjarke <bjarke@felbo.dk>
2022-03-03 11:38:40 +0000

    add PRNG using hash function for seed

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,8 @@ export interface RuneExport {
   _getScore: () => number // Provided by game
   _validateScore: (score: number) => void
   _mockEvents: () => void
+  _randomNumberGenerator: (seed: number) => () => number
+  _hashFromString: (str: string) => number
 }
 
 export const Rune: RuneExport = {
@@ -131,6 +133,35 @@ export const Rune: RuneExport = {
       }, 3000)
     }
   },
+  // A pseudorandom number generator (PRNG) for determinism.
+  // Based on the efficient mulberry32 with 32-bit state.
+  // From https://github.com/bryc/code/blob/master/jshash/PRNGs.md.
+  _randomNumberGenerator(seed: number): () => number {
+    // Initialize using hash function to avoid seed quality issues.
+    // E.g. to avoid correlations between using 1 and 2 as seed.
+    let hash = Rune._hashFromString(seed.toString())
+
+    return function() {
+      let t = hash += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+  },
+  // xmur3 from https://github.com/bryc/code/blob/master/jshash/PRNGs.md.
+  // Returns a number as opposed to seed() function for ease of use.
+  _hashFromString(str: string): number {
+    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
+      h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
+      h = h << 13 | h >>> 19;
+    }
+    const seed = () => {
+      h = Math.imul(h ^ (h >>> 16), 2246822507);
+      h = Math.imul(h ^ (h >>> 13), 3266489909);
+      return (h ^= h >>> 16) >>> 0;
+    }
+    return seed()
+  }
 }
 
 // Global namespace properties needed for communicating with Rune

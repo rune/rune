1bdbc1f1a7b86827c9e2c54e6e61ba19c979c486
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2022-05-09 20:57:36 +0300

    simplified tests

--- test/challenge.test.ts
+++ test/challenge.test.ts
@@ -1,66 +1,41 @@
 import { RuneExport, getRuneSdk } from "../src"
-import { initRune, simulateNativeApp } from "./helper"
 import { setChallengeNumber } from "../src/internal/setChallengeNumber"
 
 let Rune: RuneExport
 
 beforeEach(async () => {
-  delete globalThis.postRuneEvent
   delete globalThis._runeChallengeNumber
-  simulateNativeApp()
   Rune = getRuneSdk()
 })
 
 describe("Game Challenge", () => {
   test("should use url params to set challenge number", () => {
-    globalThis.postRuneEvent = jest.fn()
-
     setChallengeNumber(Rune, 10)
 
-    initRune(Rune)
-
-    Rune.gameOver()
-
-    expect(globalThis.postRuneEvent).toHaveBeenCalledWith({
-      type: "GAME_OVER",
-      challengeNumber: 10,
-      score: 0,
-    })
+    expect(Rune.getChallengeNumber()).toEqual(10)
   })
 
   test("should default to 1 in case the challenge number is not valid", () => {
-    globalThis.postRuneEvent = jest.fn()
-
     setChallengeNumber(Rune, NaN)
 
-    initRune(Rune)
+    expect(Rune.getChallengeNumber()).toEqual(1)
+  })
 
-    Rune.gameOver()
+  test("should default to 1 if setup is not done yet", async function () {
+    const challengeNumber = Rune.getChallengeNumber()
 
-    expect(globalThis.postRuneEvent).toHaveBeenCalledWith({
-      type: "GAME_OVER",
-      challengeNumber: 1,
-      score: 0,
-    })
+    expect(challengeNumber).toEqual(1)
   })
 
   describe("Legacy flow", () => {
     test("should always try to use window._runeChallengeNumber", () => {
-      globalThis.postRuneEvent = jest.fn()
-
       setChallengeNumber(Rune, 10)
+
+      expect(Rune.getChallengeNumber()).toEqual(10)
 
       globalThis._runeChallengeNumber = 20
 
-      initRune(Rune)
-
-      Rune.gameOver()
-
-      expect(globalThis.postRuneEvent).toHaveBeenCalledWith({
-        type: "GAME_OVER",
-        challengeNumber: 20,
-        score: 0,
-      })
+      expect(Rune.getChallengeNumber()).toEqual(20)
     })
   })
 })

37382b5bddd7909057c932942f4b78ae974660d2
Helios1138 <revan.den@gmail.com>
2022-05-23 19:17:06 +0300

    remove dist, use yarn instead of npm

--- dist/internal/setupMessageBridge.d.ts
+++
@@ -1,3 +0,0 @@
-import { RuneExport } from "../types";
-export declare function messageEventHandler(Rune: RuneExport): (event: MessageEvent) => void;
-export declare function setupMessageBridge(Rune: RuneExport, useDocumentForPostMessages: boolean): (event: MessageEvent<any>) => void;

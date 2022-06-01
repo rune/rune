a3feb3a2b9b4ed94cfed7cdb3551c7b44abbc829
Helios1138 <revan.den@gmail.com>
2022-06-01 18:13:15 +0300

    add comment

--- src/internal/setupForceMute/webAudio/interceptAudioNodeConnectToInjectMasterGainNode.ts
+++ src/internal/setupForceMute/webAudio/interceptAudioNodeConnectToInjectMasterGainNode.ts
@@ -5,6 +5,9 @@ const originalConnect = Object.getOwnPropertyDescriptor(
   "connect"
 )
 
+// We intercept the "connect() to destination" call and inject one last gain
+// node before the end of the pipeline, which then lets us adjust audio volume
+// globally
 export function interceptAudioNodeConnectToInjectMasterGainNode() {
   Object.defineProperty(AudioNode.prototype, "connect", {
     ...originalConnect,

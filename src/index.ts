b3d5617276198a7707c6ae420c39fd738441e0dd
mieszko4 <mieszko4@gmail.com>
2022-06-03 16:24:07 +0200

    2.2.0

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export function getRuneSdk({ challengeNumber }: { challengeNumber: number }) {
   const stateMachineService = createStateMachine(challengeNumber)
 
   const Rune: RuneExport = {
-    version: "2.1.1",
+    version: "2.2.0",
     init: (input) => {
       validateInput(input)
 

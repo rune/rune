e69da7a28c7caa1f550c8adbf336cb0601824309
Helios1138 <revan.den@gmail.com>
2022-06-08 17:36:03 +0300

    2.2.2

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export function getRuneSdk({ challengeNumber }: { challengeNumber: number }) {
   const stateMachineService = createStateMachine(challengeNumber)
 
   const Rune: RuneExport = {
-    version: "2.2.1",
+    version: "2.2.2",
     init: (input) => {
       validateInput(input)
 

0d21b4a2431979fc7c52dbc36b51bdf4c3b44465
Helios <revan.den@gmail.com>
2022-06-08 13:10:06 +0300

    build: add release script safety checks (#9)
    add release script safety checks

--- packages/rune-games-cli/scripts/release.sh
+++ packages/rune-games-cli/scripts/release.sh
@@ -1,9 +1,20 @@
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
+  echo "You are not on master branch";
+  exit -1
+fi
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Uncommitted changes detected";
+  exit -1
+fi
+
 echo 'Which release is it gonna be?'
 
 select version in patch minor major
 do
   case $version in
   patch|minor|major)
+    git pull
     yarn version --$version
     git push
     git push --tags

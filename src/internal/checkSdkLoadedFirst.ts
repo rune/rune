e3780712f2ccaea78b79cf25e1076d2051627bd8
Helios1138 <revan.den@gmail.com>
2022-09-06 19:55:49 +0300

    softer warning about sdk script order

--- src/internal/checkSdkLoadedFirst.ts
+++ src/internal/checkSdkLoadedFirst.ts
@@ -1,16 +1,15 @@
-import { showFullScreenError } from "./showFullScreenError"
-
 export function checkSdkLoadedFirst() {
   const scripts = document.getElementsByTagName("script")
 
   if (scripts.length !== 1) {
-    showFullScreenError({
-      header: "⚠️ Other scripts are imported before the Rune SDK script",
-      body: "Update your <pre>index.html</pre> file so that the Rune SDK script tag is included before any other scripts. This ensures that the SDK is fully loaded before the rest of the game.",
-    })
-
-    throw new Error(
-      "Rune Games SDK script tag must be included above any other scripts in index.html"
+    // eslint-disable-next-line no-console
+    console.warn(
+      "%c⚠️ Other scripts are imported before the Rune SDK script. Update " +
+        "your index.html file so that the Rune SDK script tag is " +
+        "included before any other scripts. It is also possible that this " +
+        "warning is caused by a script injected by a browser extension or an " +
+        "antivirus program. In that case  you can safely ignore this.",
+      "font-size: 16px"
     )
   }
 }

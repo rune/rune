92d395a29034364450055554922957f8243c69ca
Helios <revan.den@gmail.com>
2022-09-06 19:58:14 +0300

    Update src/internal/checkSdkLoadedFirst.ts
    Co-authored-by: Bjarke Felbo <23178145+bfelbo@users.noreply.github.com>

--- src/internal/checkSdkLoadedFirst.ts
+++ src/internal/checkSdkLoadedFirst.ts
@@ -8,7 +8,7 @@ export function checkSdkLoadedFirst() {
         "your index.html file so that the Rune SDK script tag is " +
         "included before any other scripts. It is also possible that this " +
         "warning is caused by a script injected by a browser extension or an " +
-        "antivirus program. In that case  you can safely ignore this.",
+        "antivirus program. In that case, you can safely ignore this.",
       "font-size: 16px"
     )
   }

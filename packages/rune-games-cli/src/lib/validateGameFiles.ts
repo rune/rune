fe1e1c83402b55cf689507b8dfc74aa600e18f74
Helios1138 <revan.den@gmail.com>
2022-12-20 14:27:51 +0200

    simplify validateGameFiles code, add tests

--- packages/rune-games-cli/src/lib/validateGameFiles.ts
+++ packages/rune-games-cli/src/lib/validateGameFiles.ts
@@ -1,5 +1,5 @@
 import { ESLint, Linter } from "eslint"
-import { parse, valid } from "node-html-parser"
+import { parse, valid, HTMLElement } from "node-html-parser"
 import path from "path"
 import semver from "semver"
 
@@ -33,14 +33,12 @@ export interface ValidationError {
 export interface ValidationResult {
   valid: boolean
   errors: ValidationError[]
-  multiplayer:
-    | {
-        minPlayers?: number
-        maxPlayers?: number
-        handlesPlayerJoined?: boolean
-        handlesPlayerLeft?: boolean
-      }
-    | undefined
+  multiplayer?: {
+    minPlayers?: number
+    maxPlayers?: number
+    handlesPlayerJoined?: boolean
+    handlesPlayerLeft?: boolean
+  }
 }
 
 export async function validateGameFiles(
@@ -68,177 +66,213 @@ export async function validateGameFiles(
     (file) => file.path === "logic.js" || file.path.endsWith("/logic.js")
   )
 
-  let multiplayer: ValidationResult["multiplayer"]
+  let multiplayerValidationResult: ValidationResult["multiplayer"]
 
-  if (indexHtml) {
-    if (indexHtml.content) {
-      try {
-        if (!valid(indexHtml.content)) {
-          errors.push({ message: "index.html is not valid HTML" })
-        } else {
-          const parsedIndexHtml = parse(indexHtml.content)
-          const scripts = parsedIndexHtml.getElementsByTagName("script")
-          const sdkScript = scripts.find((script) =>
-            script.getAttribute("src")?.startsWith(sdkUrlStart)
-          )
-
-          if (sdkScript) {
-            if (sdkScript.getAttribute("src")?.endsWith("/multiplayer.js")) {
-              multiplayer = {}
-
-              const logicScript = scripts.find(
-                (script) =>
-                  script.getAttribute("src") === "logic.js" ||
-                  script.getAttribute("src")?.endsWith("/logic.js")
-              )
-
-              if (logicScript) {
-                if (scripts.indexOf(logicScript) !== 1) {
-                  errors.push({
-                    message: "logic.js must be the second script in index.html",
-                  })
-                }
-
-                if (logicJs) {
-                  if (
-                    path.dirname(indexHtml.path) !== path.dirname(logicJs.path)
-                  ) {
-                    errors.push({
-                      message:
-                        "logic.js must be in the same directory as index.html",
-                    })
-                  }
-
-                  if (logicJs.content) {
-                    await eslint
-                      .lintText(logicJs.content)
-                      .then((results) => {
-                        const result = results.at(0)
-
-                        if (result) {
-                          if (result.messages.length > 0) {
-                            errors.push({
-                              message: "logic.js contains invalid code",
-                              lintErrors: result.messages,
-                            })
-                          }
-                        } else {
-                          errors.push({ message: "failed to lint logic.js" })
-                        }
-                      })
-                      .catch(() => {
-                        errors.push({ message: "failed to lint logic.js" })
-                      })
-
-                    multiplayer = extractMultiplayerMetadata(logicJs.content)
-
-                    if (
-                      !multiplayer.minPlayers ||
-                      isNaN(multiplayer.minPlayers)
-                    ) {
-                      errors.push({
-                        message: "logic.js: minPlayers not found or is invalid",
-                      })
-                    }
-
-                    if (
-                      !multiplayer.maxPlayers ||
-                      isNaN(multiplayer.maxPlayers)
-                    ) {
-                      errors.push({
-                        message: "logic.js: maxPlayers not found or is invalid",
-                      })
-                    }
-
-                    if (
-                      multiplayer.minPlayers &&
-                      (multiplayer.minPlayers < 1 || multiplayer.minPlayers > 4)
-                    ) {
-                      errors.push({
-                        message: "logic.js: minPlayers must be between 1 and 4",
-                      })
-                    }
-
-                    if (
-                      multiplayer.maxPlayers &&
-                      (multiplayer.maxPlayers < 1 || multiplayer.maxPlayers > 4)
-                    ) {
-                      errors.push({
-                        message: "logic.js: maxPlayers must be between 1 and 4",
-                      })
-                    }
-
-                    if (
-                      multiplayer.maxPlayers &&
-                      multiplayer.minPlayers &&
-                      multiplayer.maxPlayers < multiplayer.minPlayers
-                    ) {
-                      errors.push({
-                        message:
-                          "logic.js: maxPlayers must be greater than or equal to minPlayers",
-                      })
-                    }
-                  } else {
-                    errors.push({
-                      message:
-                        "logic.js content has not been provided for validation",
-                    })
-                  }
-                } else {
-                  errors.push({
-                    message: "logic.js must be included in the game files",
-                  })
-                }
-              } else {
-                errors.push({
-                  message: "logic.js file is not included in index.html",
-                })
-              }
-            }
-
-            if (scripts.indexOf(sdkScript) !== 0) {
-              errors.push({
-                message: "Rune SDK must be the first script in index.html",
-              })
-            }
-
-            const sdkVersion = sdkScript
-              .getAttribute("src")
-              ?.match(sdkVersionRegex)?.[1]
-
-            const sdkVersionCoerced = semver.coerce(sdkVersion)
-            const minSdkVersionCoerced = semver.coerce(minSdkVersion)
-
-            if (
-              sdkVersionCoerced &&
-              minSdkVersionCoerced &&
-              semver.lt(sdkVersionCoerced, minSdkVersionCoerced)
-            ) {
-              errors.push({
-                message: `Rune SDK is below minimum version (included ${sdkVersion}, min ${minSdkVersion})`,
-              })
-            }
-          } else {
-            errors.push({
-              message: "Game index.html must include Rune SDK script",
-            })
-          }
-        }
-      } catch (e) {
-        errors.push({ message: "index.html is not valid HTML" })
-      }
-    } else {
-      errors.push({
-        message: "index.html content has not been provided for validation",
-      })
-    }
-  } else {
+  if (!indexHtml) {
     errors.push({ message: "Game must include index.html" })
+  } else if (!indexHtml.content) {
+    errors.push({
+      message: "index.html content has not been provided for validation",
+    })
+  } else if (!valid(indexHtml.content)) {
+    errors.push({ message: "index.html is not valid HTML" })
+  } else {
+    const parsedIndexHtml = parse(indexHtml.content)
+    const scripts = parsedIndexHtml.getElementsByTagName("script")
+    const sdkScript = scripts.find((script) =>
+      script.getAttribute("src")?.startsWith(sdkUrlStart)
+    )
+
+    if (!sdkScript) {
+      errors.push({
+        message: "Game index.html must include Rune SDK script",
+      })
+    } else {
+      if (sdkScript.getAttribute("src")?.endsWith("/multiplayer.js")) {
+        multiplayerValidationResult = {}
+
+        await validateMultiplayer({
+          multiplayerValidationResult,
+          scripts,
+          errors,
+          logicJs,
+          indexHtml,
+        })
+      }
+
+      if (scripts.indexOf(sdkScript) !== 0) {
+        errors.push({
+          message: "Rune SDK must be the first script in index.html",
+        })
+      }
+
+      const sdkVersion = sdkScript
+        .getAttribute("src")
+        ?.match(sdkVersionRegex)?.[1]
+
+      const sdkVersionCoerced = semver.coerce(sdkVersion)
+      const minSdkVersionCoerced = semver.coerce(minSdkVersion)
+
+      if (
+        sdkVersionCoerced &&
+        minSdkVersionCoerced &&
+        semver.lt(sdkVersionCoerced, minSdkVersionCoerced)
+      ) {
+        errors.push({
+          message: `Rune SDK is below minimum version (included ${sdkVersion}, min ${minSdkVersion})`,
+        })
+      }
+    }
   }
 
   return {
     valid: errors.length === 0,
     errors,
-    multiplayer,
+    multiplayer: multiplayerValidationResult,
   }
+}
+
+async function validateMultiplayer({
+  multiplayerValidationResult,
+  scripts,
+  errors,
+  logicJs,
+  indexHtml,
+}: {
+  multiplayerValidationResult: NonNullable<ValidationResult["multiplayer"]>
+  scripts: HTMLElement[]
+  errors: ValidationError[]
+  logicJs: FileInfo | undefined
+  indexHtml: FileInfo
+}) {
+  const logicScript = scripts.find(
+    (script) =>
+      script.getAttribute("src") === "logic.js" ||
+      script.getAttribute("src")?.endsWith("/logic.js")
+  )
+
+  if (!logicScript) {
+    errors.push({
+      message: "logic.js file is not included in index.html",
+    })
+  } else {
+    if (scripts.indexOf(logicScript) !== 1) {
+      errors.push({
+        message: "logic.js must be the second script in index.html",
+      })
+    }
+
+    if (!logicJs) {
+      errors.push({
+        message: "logic.js must be included in the game files",
+      })
+    } else {
+      if (path.dirname(indexHtml.path) !== path.dirname(logicJs.path)) {
+        errors.push({
+          message: "logic.js must be in the same directory as index.html",
+        })
+      }
+
+      await validateMultiplayerLogicJsContent({
+        logicJs,
+        multiplayerValidationResult,
+        errors,
+      })
+    }
+  }
+
+  return multiplayerValidationResult
+}
+
+async function validateMultiplayerLogicJsContent({
+  logicJs,
+  multiplayerValidationResult,
+  errors,
+}: {
+  logicJs: FileInfo
+  multiplayerValidationResult: NonNullable<ValidationResult["multiplayer"]>
+  errors: ValidationError[]
+}) {
+  if (!logicJs.content) {
+    errors.push({
+      message: "logic.js content has not been provided for validation",
+    })
+  } else {
+    await eslint
+      .lintText(logicJs.content)
+      .then((results) => {
+        const result = results.at(0)
+
+        if (result) {
+          if (result.messages.length > 0) {
+            errors.push({
+              message: "logic.js contains invalid code",
+              lintErrors: result.messages,
+            })
+          }
+        } else {
+          errors.push({ message: "failed to lint logic.js" })
+        }
+      })
+      .catch(() => {
+        errors.push({ message: "failed to lint logic.js" })
+      })
+
+    Object.assign(
+      multiplayerValidationResult,
+      extractMultiplayerMetadata(logicJs.content)
+    )
+
+    if (
+      !multiplayerValidationResult.minPlayers ||
+      isNaN(multiplayerValidationResult.minPlayers)
+    ) {
+      errors.push({
+        message: "logic.js: minPlayers not found or is invalid",
+      })
+    }
+
+    if (
+      !multiplayerValidationResult.maxPlayers ||
+      isNaN(multiplayerValidationResult.maxPlayers)
+    ) {
+      errors.push({
+        message: "logic.js: maxPlayers not found or is invalid",
+      })
+    }
+
+    if (
+      multiplayerValidationResult.minPlayers &&
+      (multiplayerValidationResult.minPlayers < 1 ||
+        multiplayerValidationResult.minPlayers > 4)
+    ) {
+      errors.push({
+        message: "logic.js: minPlayers must be between 1 and 4",
+      })
+    }
+
+    if (
+      multiplayerValidationResult.maxPlayers &&
+      (multiplayerValidationResult.maxPlayers < 1 ||
+        multiplayerValidationResult.maxPlayers > 4)
+    ) {
+      errors.push({
+        message: "logic.js: maxPlayers must be between 1 and 4",
+      })
+    }
+
+    if (
+      multiplayerValidationResult.maxPlayers &&
+      multiplayerValidationResult.minPlayers &&
+      multiplayerValidationResult.maxPlayers <
+        multiplayerValidationResult.minPlayers
+    ) {
+      errors.push({
+        message:
+          "logic.js: maxPlayers must be greater than or equal to minPlayers",
+      })
+    }
+  }
+
+  return multiplayerValidationResult
 }

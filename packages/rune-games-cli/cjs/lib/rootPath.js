21d594c1bd43a5b74d3522882391e7b625cadb5f
Joel Arvidsson <joel@oblador.se>
2023-01-10 14:18:16 +0100

    fix: fix rootPath in CJS

---
+++ packages/rune-games-cli/cjs/lib/rootPath.js
@@ -0,0 +1,5 @@
+"use strict"
+Object.defineProperty(exports, "__esModule", { value: true })
+exports.rootPath = void 0
+const path = require("path")
+exports.rootPath = path.resolve(__dirname, "../..")
--- packages/rune-games-cli/src/lib/rootPath.ts
+++ packages/rune-games-cli/src/lib/rootPath.ts
@@ -1,7 +1,8 @@
 import path from "path"
 import url from "url"
 
-export const rootPath = path.resolve(
-  path.dirname(url.fileURLToPath(import.meta.url)),
-  "../.."
-)
+// eslint-disable-next-line
+// @ts-ignore
+const dirname = path.dirname(url.fileURLToPath(import.meta.url))
+
+export const rootPath = path.resolve(dirname, "../..")

5f69001b25ebbaa28af6278b854d190813438505
Helios1138 <revan.den@gmail.com>
2023-04-13 14:52:18 +0300

    remove double-click-to-zoom interaction

--- multiplayer/examples/pinpoint/src/components/OLMap/OLMap.tsx
+++ multiplayer/examples/pinpoint/src/components/OLMap/OLMap.tsx
@@ -26,6 +26,7 @@ import { Pixel } from "ol/pixel"
 import { Attribution } from "ol/control"
 import TileLayer from "ol/layer/Tile"
 import { XYZ } from "ol/source"
+import { DoubleClickZoom } from "ol/interaction"
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 useGeographic()
@@ -93,14 +94,20 @@ export const OLMap = forwardRef<
       }),
     })
 
-    setMap(
-      new Map({
-        controls: [attribution],
-        layers: [tileLayer],
-        target: containerRef.current,
-        view: new View({ center: [0, 0], zoom: 0, enableRotation: false }),
-      })
-    )
+    const map = new Map({
+      controls: [attribution],
+      layers: [tileLayer],
+      target: containerRef.current,
+      view: new View({ center: [0, 0], zoom: 0, enableRotation: false }),
+    })
+
+    map.getInteractions().forEach((interaction) => {
+      if (interaction instanceof DoubleClickZoom) {
+        map.removeInteraction(interaction)
+      }
+    })
+
+    setMap(map)
   }, [])
 
   useEffect(() => {

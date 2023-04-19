7cf8e720bbe37ed6b6af06769fe453c80eac6144
Helios1138 <revan.den@gmail.com>
2023-04-19 16:10:41 +0300

    test sudoku-gen

--- multiplayer/examples/sudoku/src/components/App.tsx
+++ multiplayer/examples/sudoku/src/components/App.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect } from "react"
 import styled from "styled-components/macro"
+import { getSudoku } from "sudoku-gen"
+
+const sudoku = getSudoku("easy")
+
+const board = sudoku.puzzle.split("").reduce((acc, cell, i) => {
+  const row = Math.floor(i / 9)
+  const col = i % 9
+
+  if (!acc[row]) acc[row] = []
+  acc[row][col] = cell
+
+  return acc
+}, [] as string[][])
 
 export function App() {
-
-
   useEffect(() => {
     Rune.initClient({
       visualUpdate: ({ newGame, players, yourPlayerId }) => {
@@ -12,11 +23,26 @@ export function App() {
     })
   }, [])
 
-
-
   return (
     <Root>
-      123
+      {board.map((row, i) => (
+        <div style={{ display: "flex" }} key={i}>
+          {row.map((cell, j) => (
+            <div
+              key={j}
+              style={{
+                width: `${100 / 9}vw`,
+                height: `${100 / 9}vw`,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {cell}
+            </div>
+          ))}
+        </div>
+      ))}
     </Root>
   )
 }

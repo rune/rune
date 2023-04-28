681f30890dbeb3c88900480e0a043504b724a0fb
Helios1138 <revan.den@gmail.com>
2023-04-28 18:18:55 +0300

    remove dev code

--- multiplayer/examples/sudoku/src/lib/boardFromSudoku.ts
+++ multiplayer/examples/sudoku/src/lib/boardFromSudoku.ts
@@ -3,22 +3,11 @@ import { Sudoku } from "sudoku-gen/dist/types/sudoku.type"
 export function boardFromSudoku(sudoku: Sudoku) {
   return sudoku.puzzle.split("").map((value, index) => ({
     value: value === "-" ? null : parseInt(value),
-    // TODO: remove, used for testing game over
-    // value: parseInt(sudoku.solution[index]),
     valueLock: Math.random(),
     fixed: value !== "-",
     correctValue: parseInt(sudoku.solution[index]),
     error: false,
     lastModifiedByPlayerId: null,
-    // TODO: remove, used for testing game over
-    // lastModifiedByPlayerId:
-    //   value !== "-"
-    //     ? null
-    //     : Object.keys(game.playerState).sort()[
-    //         Math.floor(
-    //           Math.random() * Object.keys(game.playerState).length
-    //         )
-    //       ],
     notes: [],
   }))
 }

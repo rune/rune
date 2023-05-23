90e048ae4afb9d0a5afc0dfd5357b81c64f0e5e3
Helios <revan.den@gmail.com>
2023-05-23 15:24:10 +0300

    feat: refactor examples, add sudoku example link (#153)
    refactor examples, add sudoku example link

---
+++ docs/src/pages/examples/tic-tac-toe.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { GamePage } from "../../components/GamePage"
+
+export default function TicTacToe() {
+  return <GamePage title="Tic-Tac-Toe" slug="tic-tac-toe" />
+}

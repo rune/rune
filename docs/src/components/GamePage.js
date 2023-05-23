aa21156ad7091f9054e820f608e16783c0d29b5d
Helios1138 <revan.den@gmail.com>
2023-05-23 15:20:42 +0300

    refactor examples, add sudoku example link

---
+++ docs/src/components/GamePage.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types"
+import React from "react"
+import Layout from "@theme/Layout"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+
+export function GamePage({ title, slug }) {
+  const { siteConfig } = useDocusaurusContext()
+
+  return (
+    <Layout title={title} description={siteConfig.description}>
+      <iframe
+        src={`/_examples/${slug}?embedded=1`}
+        style={{ width: "100%", height: "calc(100vh - 60px)" }}
+      />
+    </Layout>
+  )
+}
+
+GamePage.propTypes = {
+  title: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+}

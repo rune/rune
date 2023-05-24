7bd647fa23b327d9d84678ba6c052b48755583c2
Helios1138 <revan.den@gmail.com>
2023-05-24 13:48:35 +0300

    extract sounds

--- multiplayer/examples/pinpoint/src/sounds/sounds.ts
+++ multiplayer/examples/pinpoint/src/sounds/sounds.ts
@@ -1,8 +1,8 @@
 import { Howl } from "howler"
 
 export const sounds = {
-  guessSubmit: new Howl({ src: [require("./src/guessSubmit.mp3")] }),
-  otherPlayerGuess: new Howl({ src: [require("./src/otherPlayerGuess.mp3")] }),
-  resultsMap: new Howl({ src: [require("./src/resultsMap.wav")] }),
-  scoreIncrease: new Howl({ src: [require("./src/scoreIncrease.mp3")] }),
+  guessSubmit: new Howl({ src: ["sounds/guessSubmit.mp3"] }),
+  otherPlayerGuess: new Howl({ src: ["sounds/otherPlayerGuess.mp3"] }),
+  resultsMap: new Howl({ src: ["sounds/resultsMap.wav"] }),
+  scoreIncrease: new Howl({ src: ["sounds/scoreIncrease.mp3"] }),
 }

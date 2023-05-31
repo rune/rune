e47f34107b3de2c610c0492f5c95c48c49c43a7f
Joel Arvidsson <joel@rune.ai>
2023-05-31 10:36:07 +0200

    fix: use relative paths in vite template (#157)
    * fix: use relative paths in vite template
    
    * Add commetnt

--- packages/rune-games-cli/template-vite-react-ts/vite.config.ts
+++ packages/rune-games-cli/template-vite-react-ts/vite.config.ts
@@ -4,5 +4,6 @@ import rune from "vite-plugin-rune"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "", // Makes paths relative
   plugins: [react(), rune({ logicPath: "./src/logic.ts" })],
 })

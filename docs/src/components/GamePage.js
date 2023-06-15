b69a6f69c2870572ff7e95391f035be17985c271
Helios1138 <revan.den@gmail.com>
2023-06-15 19:50:23 +0300

    render demo games without footer/scroll, show github link on mobile

--- docs/src/components/GamePage.js
+++ docs/src/components/GamePage.js
@@ -1,16 +1,47 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from "@theme/Layout"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+
+const isMobile = "ontouchstart" in window
 
 export function GamePage({ title, slug }) {
   const { siteConfig } = useDocusaurusContext()
 
+  useEffect(() => {
+    if (!isMobile) return
+
+    document.body.style.overflow = "hidden"
+
+    // MutationObserver is needed because side-menu resets body overflow style
+    // when it closes, so we need to reset it back
+    const observer = new MutationObserver(() => {
+      document.body.style.overflow = "hidden"
+    })
+
+    observer.observe(document.body, {
+      attributes: true,
+      attributeFilter: ["style"],
+    })
+
+    return () => {
+      document.body.style.overflow = "auto"
+      observer.disconnect()
+    }
+  }, [])
+
   return (
-    <Layout title={title} description={siteConfig.description}>
+    <Layout
+      title={title}
+      description={siteConfig.description}
+      noFooter={isMobile}
+    >
       <iframe
         src={`/_examples/${slug}/?embedded=1`}
-        style={{ width: "100%", height: "calc(100vh - 60px)" }}
+        style={{
+          width: "100%",
+          height: "calc(100svh - var(--ifm-navbar-height))",
+        }}
       />
     </Layout>
   )

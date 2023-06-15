048cf749922c4640ecc8c735ed8cfbfdb15bf65d
Helios <revan.den@gmail.com>
2023-06-15 22:03:08 +0300

    docs: use game assets (e.g. sounds) in docs (#172)

--- docs/scripts/build-examples.sh
+++ docs/scripts/build-examples.sh
@@ -9,18 +9,18 @@ cp -r ../examples/tic-tac-toe ./static/_examples/tic-tac-toe
 # Sudoku
 cd ../examples/sudoku
 yarn
-yarn build
+../../scripts/copy-assets.sh sudoku && yarn build
 cp -r build/. ../../docs/static/_examples/sudoku
 
 # Outmatched
 cd ../outmatched
 yarn
-yarn build
+../../scripts/copy-assets.sh outmatched && yarn build
 cp -r dist/. ../../docs/static/_examples/outmatched
 perl -pe 's/multiplayer\.js/multiplayer-dev\.js/' dist/index.html > ../../docs/static/_examples/outmatched/index.html
 
 # Pinpoint
 cd ../pinpoint
 yarn
-yarn build
+../../scripts/copy-assets.sh pinpoint && yarn build
 cp -r build/. ../../docs/static/_examples/pinpoint

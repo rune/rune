c60817e9fd1ceb719c9615f5499355ef5972414f
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2023-07-18 21:15:54 +0300

    properly support windows

--- packages/rune-games-cli/template-vite-react-ts/vite.config.ts
+++ packages/rune-games-cli/template-vite-react-ts/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import rune from "vite-plugin-rune"
+import path from "node:path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "", // Makes paths relative
-  plugins: [react(), rune({ logicPath: "./src/logic.ts" })],
+  plugins: [react(), rune({ logicPath: path.resolve("./src/logic.ts") })],
 })

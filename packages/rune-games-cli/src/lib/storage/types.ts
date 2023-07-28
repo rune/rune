44265424d26d08d415a20715c3a230a1aea7972f
mieszko4 <mieszko4@gmail.com>
2023-07-28 17:30:05 +0200

    Remove lastRandomGamePort used by useGameServer

--- packages/rune-games-cli/src/lib/storage/types.ts
+++ packages/rune-games-cli/src/lib/storage/types.ts
@@ -2,7 +2,6 @@ import { Binary } from "bson"
 
 export interface Storage {
   lastRandomAppPort: number
-  lastRandomGamePort: number
   authToken: string
   installer: "yarn" | "npm" | null
   [key: string]: StorageValue<string | number | boolean | null | Date | Binary>

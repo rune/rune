73653295d662fb30396a0e411eccf1b92f5c3b81
mieszko4 <mieszko4@gmail.com>
2023-08-02 14:49:40 +0200

    feat: show QR code when using rune create and yarn dev

--- packages/rune-games-cli/template-vite-react-ts/vite.config.ts
+++ packages/rune-games-cli/template-vite-react-ts/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite"
+import { qrcode } from "vite-plugin-qrcode"
 import react from "@vitejs/plugin-react"
 import rune from "vite-plugin-rune"
 import path from "node:path"
@@ -6,5 +7,9 @@ import path from "node:path"
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "", // Makes paths relative
-  plugins: [react(), rune({ logicPath: path.resolve("./src/logic.ts") })],
+  plugins: [
+    qrcode(),
+    react(), // only applies in dev mode
+    rune({ logicPath: path.resolve("./src/logic.ts") }),
+  ],
 })

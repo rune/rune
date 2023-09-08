302b507f2ae07b192e39d5e4da86182af14c2598
mieszko4 <mieszko4@gmail.com>
2023-09-08 17:10:23 +0200

    Implement pickWeightedRandom

--- examples/pinpoint/src/lib/pickWeightedRandom.ts
+++ examples/pinpoint/src/lib/pickWeightedRandom.ts
@@ -1,4 +1,17 @@
-export function pickWeightedRandom<T>(arr: T[]) {
-  // TODO
-  return arr[Math.floor(Math.random() * arr.length)]
+type Longitude = number
+type Latitude = number
+type Weight = number // Must be integer
+type Arr = [Longitude, Latitude, Weight]
+
+export function pickWeightedRandom(arr: Arr[]) {
+  // Create weightedItems array in which every item from arr array will appear as many items as weight of that item
+  const weightedItems = arr.flatMap((item) => {
+    const [, , weight] = item
+
+    return Array(weight).fill(item)
+  })
+
+  // Pick randomly from weightedItems
+  // The chance of picking item with higher weight increases because it appears in weightedItems array more times
+  return weightedItems[Math.floor(Math.random() * weightedItems.length)]
 }

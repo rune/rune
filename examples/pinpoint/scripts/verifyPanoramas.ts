75156c6c76ba3cd80afb0ee86c9293a768856c78
mieszko4 <mieszko4@gmail.com>
2023-09-11 22:34:26 +0200

    Simplify

--- examples/pinpoint/scripts/verifyPanoramas.ts
+++ examples/pinpoint/scripts/verifyPanoramas.ts
@@ -1,33 +1,6 @@
 import * as allPanoramas from "../src/lib/data/panoramas.json"
 import * as assert from "assert"
 
-import { MathUtils as MathUtils6 } from "three"
-
-// Copied from src/components/PanoramaView/PhotoSphereViewer.tsx
-const zoomRanges = [
-  [180, 50],
-  [50, 35],
-  [35, 15],
-  [15, 0],
-]
-export function remap(
-  value: number,
-  [originalMin, originalMax]: [number, number],
-  [newMin, newMax]: [number, number]
-) {
-  return (
-    newMin +
-    ((value - originalMin) * (newMax - newMin)) / (originalMax - originalMin)
-  )
-}
-function mapFovToZoom(
-  fov: number,
-  { minFov, maxFov }: { minFov: number; maxFov: number }
-) {
-  const zoom = remap(fov, [minFov, maxFov], [100, 0])
-  return zoom > 100 ? 100 : zoom < 0 ? 0 : zoom
-}
-
 function verifyPanoramas() {
   for (const item of allPanoramas) {
     // fov
@@ -36,28 +9,6 @@ function verifyPanoramas() {
         typeof item.view.minFov === "number" &&
         typeof item.view.maxFov === "number"
     )
-    // https://photo-sphere-viewer.js.org/guide/config.html#standard-options
-    assert(item.view.maxFov > item.view.minFov && item.view.maxFov < 180)
-    assert(item.view.minFov > 1 && item.view.minFov < item.view.maxFov)
-
-    assert(!Number.isNaN(MathUtils6.degToRad(item.view.fov)))
-    assert(!Number.isNaN(MathUtils6.degToRad(item.view.maxFov)))
-    assert(!Number.isNaN(MathUtils6.degToRad(item.view.minFov)))
-
-    // src/components/PanoramaView/PhotoSphereViewer.tsx
-    assert(item.levels.length > 0 && item.levels.length <= 3)
-    item.levels.forEach((level, i) => {
-      const zoomRange = [
-        zoomRanges[i][0],
-        i === item.levels.length - 1 ? 0 : zoomRanges[i][1],
-      ]
-
-      const z1 = mapFovToZoom(zoomRange[0], item.view)
-      const z2 = mapFovToZoom(zoomRange[1], item.view)
-
-      assert(z1 >= 0 && z1 <= 100)
-      assert(z2 >= 0 && z2 <= 100)
-    })
 
     // yaw & pitch
     assert(

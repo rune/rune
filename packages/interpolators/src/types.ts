3d5675a5e1d7d3f9999ef3e84b90444a43ee52e4
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2023-09-28 15:56:43 +0300

    type

--- packages/interpolators/src/types.ts
+++ packages/interpolators/src/types.ts
@@ -1,4 +1,4 @@
 export type Interpolator = <Dimensions extends number | number[]>() => {
-  update: (params: { current: Dimensions; next: Dimensions }) => void
+  update: (params: { current: Dimensions; future: Dimensions }) => void
   getPosition: () => Dimensions
 }

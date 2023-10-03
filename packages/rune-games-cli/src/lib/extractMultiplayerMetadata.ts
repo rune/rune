745fcd6f2cff1e700b4656fad5de7472177c6d3a
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2023-10-03 12:27:00 +0100

    feat: update per second/input delay config option (#213)

--- packages/rune-games-cli/src/lib/extractMultiplayerMetadata.ts
+++ packages/rune-games-cli/src/lib/extractMultiplayerMetadata.ts
@@ -3,6 +3,8 @@ const regexes = {
   maxPlayers: /maxPlayers\s*:\s*?([0-9]+)/,
   playerJoined: /playerJoined\s*[:(]/,
   playerLeft: /playerLeft\s*[:(]/,
+  updatesPerSecond: /updatesPerSecond\s*:\s*?([0-9]+)/,
+  inputDelay: /inputDelay\s*:\s*?([0-9]+)/,
 }
 
 export function extractMultiplayerMetadata(logicJsContent: string) {
@@ -12,14 +14,26 @@ export function extractMultiplayerMetadata(logicJsContent: string) {
   const maxPlayersString = logicJsContent.match(regexes.maxPlayers)?.at(1)
   const maxPlayers = maxPlayersString ? parseInt(maxPlayersString) : undefined
 
+  const updatesPerSecondString = logicJsContent
+    .match(regexes.updatesPerSecond)
+    ?.at(1)
+  const updatesPerSecond = updatesPerSecondString
+    ? parseInt(updatesPerSecondString)
+    : undefined
+
   const handlesPlayerJoined = regexes.playerJoined.test(logicJsContent)
 
   const handlesPlayerLeft = regexes.playerLeft.test(logicJsContent)
+
+  const inputDelayString = logicJsContent.match(regexes.inputDelay)?.at(1)
+  const inputDelay = inputDelayString ? parseInt(inputDelayString) : undefined
 
   return {
     minPlayers,
     maxPlayers,
     handlesPlayerJoined,
     handlesPlayerLeft,
+    updatesPerSecond,
+    inputDelay,
   }
 }

0427f110514f72365966f724ba6fe95e7fa659b2
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2023-10-16 12:54:05 +0100

    refactor: add eslint rules in every example game (#242)

--- examples/pinpoint/.eslintrc.cjs
+++ examples/pinpoint/.eslintrc.cjs
@@ -4,8 +4,15 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:rune/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
 }

adf783a58170a423ddc42f9b1c78c73e94bf6db1
Helios1138 <revan.den@gmail.com>
2023-10-18 18:02:08 +0300

    more header ui

--- examples/snake/src/components/Header/Header.tsx
+++ examples/snake/src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import { styled } from "styled-components"
 import { useAtomValue } from "jotai"
-import { $game, $players, $yourPlayerId } from "../state/state.ts"
-import { rel } from "../lib/rel.ts"
+import { $game, $players, $yourPlayerId } from "../../state/state.ts"
+import { rel } from "../../lib/rel.ts"
+import noAvatar from "./noAvatar.png"
+import { useMemo } from "react"
+import { pickFreeColor } from "../../logic/pickFreeColor.ts"
 
 export function Header() {
   // TODO: think how to avoid re-rendering on every tick because we use the full game state
@@ -9,23 +12,39 @@ export function Header() {
   const players = useAtomValue($players)
   const yourPlayerId = useAtomValue($yourPlayerId)
 
+  const invite = useMemo(
+    () =>
+      Object.keys(players).length < 4 ? { color: pickFreeColor(game) } : null,
+    [game, players],
+  )
+
   return (
     <Root>
-      {Object.values(game.players).map(({ playerId, color, score, state }) => (
+      {game.players.map(({ playerId, color, score, state }) => (
         <PlayerContainer key={playerId}>
           {state === "pending" ? (
-            <div style={{ color: "white" }}>pending</div>
+            <DarkCircle $playerColor={color} style={{ color: "white" }}>
+              pending
+            </DarkCircle>
           ) : state === "dead" ? (
-            <div style={{ color: "white" }}>dead</div>
+            <DarkCircle $playerColor={color} style={{ color: "white" }}>
+              dead
+            </DarkCircle>
           ) : (
             <Avatar src={players[playerId].avatarUrl} $playerColor={color} />
           )}
           <Name $playerColor={color}>
             {playerId === yourPlayerId ? "You" : players[playerId].displayName}
           </Name>
-          <Score $playerColor={color}>{score}</Score>
+          {state === "alive" && <Score $playerColor={color}>{score}</Score>}
         </PlayerContainer>
       ))}
+      {invite && (
+        <PlayerContainer onClick={() => Rune.showInvitePlayers()}>
+          <Avatar src={noAvatar} $playerColor={pickFreeColor(game)} />
+          <Invite $playerColor={invite.color}>Invite</Invite>
+        </PlayerContainer>
+      )}
     </Root>
   )
 }
@@ -66,6 +85,10 @@ const Name = styled.div<{ $playerColor: string }>`
   line-height: normal;
 `
 
+const Invite = styled(Name)`
+  font-size: ${rel(14)};
+`
+
 const Score = styled.div<{ $playerColor: string }>`
   color: ${({ $playerColor }) => $playerColor};
   text-shadow: 0 0 ${rel(15)} ${({ $playerColor }) => $playerColor};
@@ -74,4 +97,11 @@ const Score = styled.div<{ $playerColor: string }>`
   font-size: ${rel(24)};
   height: ${rel(21)};
   line-height: ${rel(21)}; // TODO: this is not right
+`
+
+const DarkCircle = styled.div<{ $playerColor: string }>`
+  width: ${rel(66)};
+  height: ${rel(66)};
+  border-radius: 50%;
+  box-shadow: 0 0 ${rel(15)} ${rel(3)} ${({ $playerColor }) => $playerColor};
 `

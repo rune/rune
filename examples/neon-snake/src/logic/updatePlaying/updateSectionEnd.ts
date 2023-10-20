2f65c9e3028b5128e55a356312c5e6246415a594
Helios1138 <revan.den@gmail.com>
2023-10-20 16:52:38 +0300

    simplify updateSectionEnd

--- examples/neon-snake/src/logic/updatePlaying/updateSectionEnd.ts
+++ examples/neon-snake/src/logic/updatePlaying/updateSectionEnd.ts
@@ -3,17 +3,16 @@ import { turnDegreesPerTick, movePixelsPerTick } from "../logicConfig.ts"
 import { degreesToRad } from "../../lib/degreesToRad.ts"
 
 export function updateSectionEnd(section: Section) {
-  const turningModifier =
-    section.turning === "none" ? 0 : section.turning === "right" ? 1 : -1
-
-  section.endAngle += turnDegreesPerTick * turningModifier
-
-  section.end.x += Math.cos(degreesToRad(section.endAngle)) * movePixelsPerTick
-  section.end.y += Math.sin(degreesToRad(section.endAngle)) * movePixelsPerTick
-
   if (section.turning !== "none") {
+    const turningModifier = section.turning === "right" ? 1 : -1
+
+    section.endAngle += turnDegreesPerTick * turningModifier
+
     section.arc.endAngle = degreesToRad(
       section.endAngle + (-90 + turnDegreesPerTick / 2) * turningModifier,
     )
   }
+
+  section.end.x += Math.cos(degreesToRad(section.endAngle)) * movePixelsPerTick
+  section.end.y += Math.sin(degreesToRad(section.endAngle)) * movePixelsPerTick
 }

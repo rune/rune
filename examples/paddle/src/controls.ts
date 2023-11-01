cb0de9507e09ea3d430d11a51c767f6fd4f8db68
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2023-11-01 14:36:28 +0000

    feat: paddle example game improvements (#266)

--- examples/paddle/src/controls.ts
+++ examples/paddle/src/controls.ts
@@ -1,21 +1,63 @@
 import { throttle } from "./helpers.ts"
 import { GAME_WIDTH, PADDLE_WIDTH } from "./logic.ts"
 
-export function initControls() {
-  const move = throttle((x: number) => {
-    const position = Math.max(
+let controlsInitialized = false
+
+export function initControls(
+  onMove: () => void,
+  getIsPlayer: () => boolean,
+  getPaddlePosition: () => number
+) {
+  if (controlsInitialized) {
+    return
+  }
+
+  controlsInitialized = true
+
+  let allowMove = false
+
+  const calculatePosition = (x: number) => {
+    return Math.max(
       0,
       Math.min(
         Math.round((x / window.innerWidth) * GAME_WIDTH - PADDLE_WIDTH / 2),
         GAME_WIDTH - PADDLE_WIDTH
       )
     )
+  }
 
-    Rune.actions.setPosition(position)
+  const move = throttle((position: number) => {
+    if (getIsPlayer()) {
+      Rune.actions.setPosition(position)
+    }
   }, 100)
 
-  window.addEventListener("pointerdown", (event) => move(event.clientX))
+  window.addEventListener("pointerdown", (event) => {
+    const paddle = getPaddlePosition()
+
+    const cursor = calculatePosition(event.clientX)
+
+    const distanceFromPaddleCenter = Math.abs(paddle - cursor)
+
+    if (distanceFromPaddleCenter < PADDLE_WIDTH * 1.4) {
+      allowMove = true
+      move(cursor)
+    } else {
+      allowMove = false
+    }
+  })
+
+  window.addEventListener("pointerup", () => {
+    allowMove = false
+  })
+
   window.addEventListener("pointermove", (event) => {
-    move(event.clientX)
+    if (allowMove) {
+      const position = calculatePosition(event.clientX)
+
+      move(position)
+
+      onMove()
+    }
   })
 }

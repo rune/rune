5a7a63bf0b9c3beb4ddb4e8e9906dd140add78b1
Helios1138 <revan.den@gmail.com>
2023-12-06 15:27:16 +0200

    mobile footer, hero, phone

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -328,6 +328,53 @@ export default function Home() {
     window.addEventListener("scroll", onScroll)
     return () => window.removeEventListener("scroll", onScroll)
   }, [])
+  const footerCopyright = <>© {new Date().getFullYear()} Rune</>
+
+  const footerLinks = (
+    <>
+      <Link className={styles.link} to="/docs/examples">
+        Examples
+      </Link>
+      <a className={styles.link} href="https://rune.ai" target="_blank">
+        Get Rune
+      </a>
+      <a className={styles.link} href="https://rune.ai/team" target="_blank">
+        About
+      </a>
+    </>
+  )
+
+  const footerSocial = (
+    <>
+      <a href="https://github.com/rune/rune-games-sdk" target="_blank">
+        <img
+          alt="GitHub Logo"
+          src={
+            require("!!url-loader!@site/static/img/home/social/github.svg")
+              .default
+          }
+        />
+      </a>
+      <a href="https://discord.gg/rune-devs" target="_blank">
+        <img
+          alt="Discord Logo"
+          src={
+            require("!!url-loader!@site/static/img/home/social/discord.svg")
+              .default
+          }
+        />
+      </a>
+      <a href="https://twitter.com/joinrune" target="_blank">
+        <img
+          alt="Twitter Logo"
+          src={
+            require("!!url-loader!@site/static/img/home/social/twitter.svg")
+              .default
+          }
+        />
+      </a>
+    </>
+  )
 
   return (
     <div className={styles.home}>
@@ -421,64 +468,18 @@ export default function Home() {
       <div className={styles.footer}>
         <div className={styles.line}>
           <div className={clsx(styles.link, styles.side)}>
-            © {new Date().getFullYear()} Rune
+            {footerCopyright}
           </div>
-          <div className={styles.center}>
-            <Link className={styles.link} to="/docs/examples">
-              Examples
-            </Link>
-            <a className={styles.link} href="https://rune.ai" target="_blank">
-              Get Rune
-            </a>
-            <a
-              className={styles.link}
-              href="https://rune.ai/team"
-              target="_blank"
-            >
-              About
-            </a>
-          </div>
-          <div className={clsx(styles.side, styles.social)}>
-            <a href="https://github.com/rune/rune-games-sdk" target="_blank">
-              <img
-                alt="GitHub Logo"
-                src={
-                  require("!!url-loader!@site/static/img/home/social/github.svg")
-                    .default
-                }
-              />
-            </a>
-            <a href="https://discord.gg/rune-devs" target="_blank">
-              <img
-                alt="Discord Logo"
-                src={
-                  require("!!url-loader!@site/static/img/home/social/discord.svg")
-                    .default
-                }
-              />
-            </a>
-            <a href="https://twitter.com/joinrune" target="_blank">
-              <img
-                alt="Twitter Logo"
-                src={
-                  require("!!url-loader!@site/static/img/home/social/twitter.svg")
-                    .default
-                }
-              />
-            </a>
-          </div>
+          <div className={styles.center}>{footerLinks}</div>
+          <div className={clsx(styles.side, styles.social)}>{footerSocial}</div>
         </div>
+      </div>
+
+      <div className={styles.footerMobile}>
+        <div className={styles.links}>{footerLinks}</div>
+        <div className={styles.social}>{footerSocial}</div>
+        <div className={styles.link}>{footerCopyright}</div>
       </div>
     </div>
   )
 }
-
-/*
-SELECT
-	display_name,
-	'https://app.rune.ai/avatar?base=' || avatar_base_id || '&hair=' || avatar_hair_id || '&tilt=' || avatar_tilt || '&headgear=' || avatar_headgear_id || '&accessory=' || avatar_accessory_id || '&background=' || avatar_background_id || '&expression=' || avatar_expression_id || '&size=90&isCropped=1&isBackgroundTransparent=0' avatar
-FROM
-	tango.user_profile_editable
-WHERE
-	display_name ILIKE '%helios%';
- */

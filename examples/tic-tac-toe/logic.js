9f4d12579ed27d54bfffea32918275e0efc3f286
bfelbo <23178145+bfelbo@users.noreply.github.com>
2023-12-14 16:04:17 +0100

    simplify findWinningCombo

--- examples/tic-tac-toe/logic.js
+++ examples/tic-tac-toe/logic.js
@@ -7,30 +7,6 @@ function setup(allPlayerIds) {
   }
 }
 
-const findWinningCombo = (cells) => {
-  const WINNING_COMBOS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-  for (const combo of WINNING_COMBOS) {
-    const [i1, i2, i3] = combo
-    if (
-      cells[i1] !== null &&
-      cells[i1] === cells[i2] &&
-      cells[i1] === cells[i3]
-    ) {
-      return combo
-    }
-  }
-  return null
-}
-
 function claimCell(cellIndex, { game, playerId }) {
   // Cannot play during someone else's turn or claim existing cells
   if (game.cells[cellIndex] !== null || playerId === game.lastPlayerId) {
@@ -59,6 +35,11 @@ function claimCell(cellIndex, { game, playerId }) {
       },
     })
   }
+}
+
+function findWinningCombo (cells) {
+  return [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+      .find(combo => combo.every(i => cells[i] && cells[i] === cells[combo[0]])) || null;
 }
 
 Rune.initLogic({

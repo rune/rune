0a21888fddcde406929006d796b26458a281549b
Helios <revan.den@gmail.com>
2023-12-15 11:49:40 +0200

    Tic-Tac-Toe: disable cells when it's not your turn (#285)
    * disable cells
    
    * add into new line
    
    ---------
    
    Co-authored-by: bfelbo <23178145+bfelbo@users.noreply.github.com>

--- examples/tic-tac-toe/client.js
+++ examples/tic-tac-toe/client.js
@@ -15,13 +15,12 @@ function initUI(cells, playerIds, players, yourPlayerId) {
   playerContainers = playerIds.map((playerId, index) => {
     const li = document.createElement("li")
     li.setAttribute("player", index)
-    li.innerHTML = `
-           <img src="${players[playerId].avatarUrl}" />
+    li.innerHTML =
+           `<img src="${players[playerId].avatarUrl}" />
            <span>${
              players[playerId].displayName +
              (players[playerId].playerId === yourPlayerId ? " (You)" : "")
-           }</span>
-         `
+           }</span>`
     playersSection.appendChild(li)
     return li
   })
@@ -29,22 +28,22 @@ function initUI(cells, playerIds, players, yourPlayerId) {
 
 function onChange({ game, players, yourPlayerId, action }) {
   const { cells, playerIds, winCombo, lastMovePlayerId, freeCells } = game
-
   if (!cellButtons) initUI(cells, playerIds, players, yourPlayerId)
 
   cellButtons.forEach((button, i) => {
     button.setAttribute("player", playerIds.indexOf(cells[i]))
-    button.setAttribute("clickable", !cells[i] && lastMovePlayerId !== yourPlayerId)
     button.setAttribute("dim", (winCombo && !winCombo.includes(i)) || !freeCells)
+    if(cells[i] || lastMovePlayerId === yourPlayerId)
+      button.setAttribute("disabled", "")
+    else
+      button.removeAttribute("disabled")
   })
 
   playerContainers.forEach((container, i) => {
     container.setAttribute("your-turn", playerIds[i] !== lastMovePlayerId)
   })
 
-  if (action && action.name === "claimCell") {
-    selectSound.play()
-  }
+  if (action && action.name === "claimCell") selectSound.play()
 }
 
 Rune.initClient({ onChange })

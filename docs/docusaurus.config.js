7ee44e845f7dce5cffea121ec052715133670933
bfelbo <23178145+bfelbo@users.noreply.github.com>
2023-12-18 11:55:29 +0100

    rename to "Rune" + add title description

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula")
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Rune SDK",
+  title: "Rune - multiplayer web games",
   tagline: "Make multiplayer games that reach a community of millions.",
   url: "http://developers.rune.ai/",
   baseUrl: "/",

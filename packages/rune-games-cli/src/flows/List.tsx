9859803e050ea6dd34e71cc629802265e92dd406
mieszko4 <mieszko4@gmail.com>
2024-01-02 17:51:05 +0100

    Use interactive list

--- packages/rune-games-cli/src/flows/List.tsx
+++ packages/rune-games-cli/src/flows/List.tsx
@@ -1,38 +1,46 @@
-import figures from "figures"
 import { Text, Box } from "ink"
-import React from "react"
+import React, { useMemo, useState } from "react"
 
+import { Select } from "../components/Select.js"
 import { useGames, useMyGames, gameItemLabel } from "../gql/useGames.js"
 import { useMe } from "../gql/useMe.js"
 
 export function List() {
   const { me } = useMe()
-  const { games } = useGames({ skip: !me })
+  const { games, gamesLoading } = useGames({ skip: !me })
   const { myGames } = useMyGames({ games, devId: me?.devId })
+  const [gameId, setGameId] = useState<number | null>(null)
+
+  const items = useMemo(
+    () =>
+      me?.admin
+        ? (games ?? []).map((game) => ({
+            label: gameItemLabel({ game, showGameDevs: true }),
+            value: game.id,
+          }))
+        : (myGames ?? []).map((game) => ({
+            label: gameItemLabel({ game, showGameDevs: false }),
+            value: game.id,
+          })),
+    [games, myGames, me]
+  )
 
   if (!me) return <></>
 
   return (
     <Box flexDirection="column">
-      <Text bold>Your games:</Text>
-      {myGames?.length ? (
-        myGames.map((game) => (
-          <Text key={game.id}>
-            {figures.bullet} {gameItemLabel({ game, showGameDevs: false })}
-          </Text>
-        ))
+      <Text bold>{me?.admin ? "All games:" : "Your games:"}</Text>
+      {gamesLoading ? (
+        <Text dimColor>Loading...</Text>
+      ) : items.length > 0 ? (
+        <Select
+          items={items}
+          value={gameId}
+          onChange={setGameId}
+          onSubmit={() => console.log("SHOW IT", gameId)}
+        />
       ) : (
         <Text dimColor>You have not uploaded any games yet</Text>
-      )}
-      {me?.admin && (
-        <>
-          <Text bold>All games:</Text>
-          {games?.map((game) => (
-            <Text key={game.id}>
-              {figures.bullet} {gameItemLabel({ game, showGameDevs: true })}
-            </Text>
-          ))}
-        </>
       )}
     </Box>
   )

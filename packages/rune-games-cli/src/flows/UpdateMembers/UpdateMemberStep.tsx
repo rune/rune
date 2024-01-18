65d0ca07d3c6ef161ee9cc0d5aef1611c4e16c41
mieszko4 <mieszko4@gmail.com>
2024-01-18 18:12:18 +0100

    feat: add human err when updating admin of the game

--- packages/rune-games-cli/src/flows/UpdateMembers/UpdateMemberStep.tsx
+++ packages/rune-games-cli/src/flows/UpdateMembers/UpdateMemberStep.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react"
 import { Step } from "../../components/Step.js"
 import { GameDevType } from "../../generated/types.js"
 import { useUpdateGameDev } from "../../gql/useUpdateGameDev.js"
+import { formatApolloError } from "../../lib/formatApolloError.js"
 
 export function UpdateMemberStep({
   gameId,
@@ -37,7 +38,11 @@ export function UpdateMemberStep({
         updateGameDevLoading
           ? "Updating the member"
           : updateGameDevError
-          ? "Something went wrong"
+          ? formatApolloError(updateGameDevError, {
+              "[tango][GAME_DEV_NO_ACTIVE_ADMIN]":
+                "Failed to update because there would be no active admin for this game",
+              default: "Something went wrong",
+            })
           : memberType === null
           ? "Member removed"
           : "Member updated"

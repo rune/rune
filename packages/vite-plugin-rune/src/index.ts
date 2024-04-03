4103c99e74d1eed462d66220ef84d660466f599f
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2024-04-03 16:09:31 +0300

    logic path check

--- packages/vite-plugin-rune/src/index.ts
+++ packages/vite-plugin-rune/src/index.ts
@@ -15,6 +15,13 @@ export default function runePlugin(options: ViteRunePluginOptions): Plugin[] {
   if (!options?.logicPath) {
     throw new Error("`logicPath` option is required to use the Rune plugin")
   }
+
+  if (!options.logicPath.includes("logic")) {
+    throw new Error(
+      "`logicPath` must contain `logic` in its path. For example: `src/logic.ts`, `src/logic.js`, `src/logic/index.ts`, `src/logic/index.js`"
+    )
+  }
+
   const logicPath = path.resolve(options.logicPath)
   if (!existsSync(logicPath)) {
     throw new Error(`Rune logic file "${options.logicPath}" not found`)

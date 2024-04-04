f036206de2ca12584c531313aa9e89beb4eeb360
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2024-04-04 17:50:07 +0300

    use new plugin

--- packages/dusk-cli/templates/typescript-react/vite.config.ts
+++ packages/dusk-cli/templates/typescript-react/vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react"
 import path from "node:path"
 import { defineConfig } from "vite"
 import { qrcode } from "vite-plugin-qrcode"
-import rune from "vite-plugin-rune"
+import dusk from "vite-plugin-dusk"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     qrcode(), // only applies in dev mode
     react(),
-    rune({
+    dusk({
       logicPath: path.resolve("./src/logic.ts"),
       minifyLogic: false, // This flag can be used if your logic reaches the allowed limit. However, it will make it significantly more difficult to detect validation issues
       ignoredDependencies: [],
--- packages/dusk-cli/templates/typescript/vite.config.ts
+++ packages/dusk-cli/templates/typescript/vite.config.ts
@@ -1,14 +1,14 @@
 import path from "node:path"
 import { defineConfig } from "vite"
 import { qrcode } from "vite-plugin-qrcode"
-import rune from "vite-plugin-rune"
+import dusk from "vite-plugin-dusk"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "", // Makes paths relative
   plugins: [
     qrcode(), // only applies in dev mode
-    rune({
+    dusk({
       logicPath: path.resolve("./src/logic.ts"),
       minifyLogic: false, // This flag can be used if your logic reaches the allowed limit. However, it will make it significantly more difficult to detect validation issues
       ignoredDependencies: [],

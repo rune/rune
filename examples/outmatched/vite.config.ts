2d1f9710410f9480271ee31570ec0ea3128cd823
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2024-04-08 16:57:05 +0300

    update example games

--- examples/outmatched/vite.config.ts
+++ examples/outmatched/vite.config.ts
@@ -4,5 +4,5 @@ import rune from "vite-plugin-rune"
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "", // Makes paths relative, so that the game can be hosted on developers.rune.ai
-  plugins: [rune({ logicPath: "./src/logic/index.ts" })],
+  plugins: [rune({ logicPath: "./src/logic/logic.ts" })],
 })

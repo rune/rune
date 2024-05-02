b7a6fdfbf7c218e94b9bb2a6a6d7e4e80a431247
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2024-05-02 14:46:18 +0100

    fix: logic client html (#369)
    * logic client html
    
    * version
    
    * apply only on build
    
    * add tag to disable warning
    
    * comment

---
+++ packages/vite-plugin-dusk/src/plugins/devPlugins.ts
@@ -0,0 +1,55 @@
+import { readFileSync } from "node:fs"
+import path from "node:path"
+import type { Plugin } from "vite"
+import crypto from "crypto"
+
+const runtimePublicPath = "/@dusk-games-sdk"
+
+export function getDevPlugins(duskPkgPath: string): Plugin[] {
+  return [
+    {
+      name: "vite:dusk-plugin:resolve-runtime",
+      apply: "serve",
+      enforce: "pre", // Run before Vite default resolve to avoid syscalls
+      resolveId: (id) => (id === runtimePublicPath ? id : undefined),
+      load: (id) =>
+        id === runtimePublicPath
+          ? readFileSync(
+              path.resolve(duskPkgPath, "../multiplayer-dev.js"),
+              "utf-8"
+            )
+          : undefined,
+    },
+    {
+      name: "vite:dusk-plugin:inject-runtime",
+      apply: "serve",
+      enforce: "post",
+      transformIndexHtml(html) {
+        return {
+          html,
+          tags: [
+            // Inject a tag to differentiate between different games when running in dev mode
+            {
+              tag: "script",
+              attrs: {
+                "data-rune-allow-before-sdk": "1",
+              },
+              children: `window.__SDK_SETTINGS_ID__='${crypto
+                .createHash("shake256", { outputLength: 8 })
+                .update(process.cwd())
+                .digest("hex")}'`,
+              injectTo: "head-prepend",
+            },
+            {
+              tag: "script",
+              attrs: {
+                src: runtimePublicPath,
+              },
+              injectTo: "head-prepend",
+            },
+          ],
+        }
+      },
+    },
+  ]
+}
---
+++ packages/vite-plugin-dusk/src/plugins/transformHtml.ts
@@ -0,0 +1,52 @@
+import { readFileSync } from "node:fs"
+import type { Plugin } from "vite"
+
+export function getTransformHtmlForBuildPlugins(duskPkgPath: string): Plugin[] {
+  const duskVersion = JSON.parse(readFileSync(duskPkgPath, "utf-8")).version
+
+  return [
+    {
+      name: "vite:dusk-plugin:inject-runtime",
+      apply: "build",
+      enforce: "post",
+      transformIndexHtml(html) {
+        return {
+          //Remove the inlined logic file in case of build.
+          //Vite puts the chunks as link preload tags in html, and we want them to be simple script tags.
+          //Also remove client, so that logic & client scripts are next to each other
+          html: html
+            .replace(/<link.* href="(\.)*\/logic\.js">/, "")
+            .replace(/<script.* src="(\.)*\/client\.js"><\/script>/, ""),
+          tags: [
+            {
+              tag: "script",
+              attrs: {
+                src: `https://cdn.jsdelivr.net/npm/dusk-games-sdk@${duskVersion}/multiplayer.js`,
+              },
+              injectTo: "head-prepend",
+            },
+            //Actually insert the logic.js & client.js script tag.
+            {
+              tag: "script",
+              attrs: {
+                type: "module",
+                crossorigin: true,
+                src: "./logic.js",
+              },
+              injectTo: "head-prepend",
+            },
+            {
+              tag: "script",
+              attrs: {
+                type: "module",
+                crossorigin: true,
+                src: "./client.js",
+              },
+              injectTo: "head-prepend",
+            },
+          ],
+        }
+      },
+    },
+  ]
+}

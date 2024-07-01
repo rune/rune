4a598ed55b668e118a0f179419fe1eed792c9050
Coke And Code <kevin@rune.ai>
2024-07-01 13:25:42 +0200

    Feat: deceleration interpolators (#427)
    * Latency interpolated to account for moving in more than one axis/component at the same time
    
    * Fix lint
    
    * Add deceleration to interpolators
    
    * Update test to do both single and multiple component tests

--- packages/interpolators/src/interpolators/interpolatorLatency.ts
+++ packages/interpolators/src/interpolators/interpolatorLatency.ts
@@ -7,6 +7,15 @@ const runValidation = true
 function moveTowardsSingleValue(from: number, to: number, maxSpeed: number) {
   const distance = to - from
 
+  // max speed can be the opposite sign of the direction we want
+  // to travel, this will mean we're in deceleration
+  if (Math.sign(maxSpeed) !== Math.sign(distance)) {
+    // we're decelerating so just move the speed given
+    return from + maxSpeed
+  }
+
+  // otherwise we're speeding in the right direction so pick either
+  // move speed or if we're close enough just move to the target
   const distanceToMove = Math.min(Math.abs(distance), Math.abs(maxSpeed))
 
   return from + distanceToMove * Math.sign(distance)
@@ -55,16 +64,23 @@ export function interpolatorLatency<Dimensions extends number | number[]>({
   function calculateSpeedSingleValue(
     interpolated: number,
     target: number,
-    currentSpeed: number
+    currentSpeed: number,
+    componentCoefficient: number
   ) {
     if (!useAcceleration) {
-      return maxSpeed
+      return maxSpeed * componentCoefficient
     }
 
     if (target > interpolated) {
-      return Math.min(Math.max(currentSpeed, 0) + acceleration, maxSpeed)
+      return Math.min(
+        currentSpeed + acceleration * componentCoefficient,
+        maxSpeed * componentCoefficient
+      )
     } else if (target < interpolated) {
-      return Math.max(Math.min(currentSpeed, 0) - acceleration, -maxSpeed)
+      return Math.max(
+        currentSpeed - acceleration * componentCoefficient,
+        -maxSpeed * componentCoefficient
+      )
     } else {
       return 0
     }
@@ -84,14 +100,23 @@ export function interpolatorLatency<Dimensions extends number | number[]>({
       return calculateSpeedSingleValue(
         interpolated as number,
         target as number,
-        speed as number
+        speed as number,
+        1
       ) as Dimensions
     } else {
+      const valueDeltas = (target as number[]).map(
+        (value, index) => value - interpolated[index]
+      )
+      const totalLength = Math.sqrt(
+        valueDeltas.map((d) => d ** 2).reduce((p, v) => p + v, 0)
+      )
+
       return (interpolated as number[]).map((interpolatedValue, index) => {
         return calculateSpeedSingleValue(
           interpolatedValue,
           (target as number[])[index],
-          (speed as number[])[index] ?? 0
+          (speed as number[])[index] ?? 0,
+          Math.abs(valueDeltas[index]) / totalLength
         )
       }) as Dimensions
     }
@@ -130,7 +155,6 @@ export function interpolatorLatency<Dimensions extends number | number[]>({
       }
 
       speed = calculateSpeed(interpolated, params.game)
-
       interpolated = moveTowards(
         interpolated,
         params.game,

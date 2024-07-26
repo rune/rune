9f72286f35b594a831840d0bd80b819d097db0bb
mieszko4 <mieszko4@gmail.com>
2024-07-26 08:44:24 +0200

    refactor: move out phoneVideos

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useCallback } from "react"
+import PropTypes from "prop-types"
 import clsx from "clsx"
 import Link from "@docusaurus/Link"
 import { Player } from "@lottiefiles/react-lottie-player"
@@ -9,6 +10,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import Layout from "@theme/Layout"
 import { HomeGameDemo } from "../components/HomeGameDemo/HomeGameDemo"
 import demoCode from "./demoCode.json"
+import { gameVideos } from "../components/gameVideos"
 
 const features = [
   {
@@ -53,164 +55,9 @@ const features = [
   },
 ]
 
-const videos = [
-  {
-    game: {
-      title: "Spooky Cookie",
-      developers: [
-        {
-          name: "alexwoods",
-          avatar:
-            "https://app.dusk.gg/avatar?base=2&hair=29&tilt=1&headgear=55&accessory=64&background=72&expression=18&size=90&isCropped=1&isBackgroundTransparent=0",
-        },
-      ],
-      minPlayers: 1,
-      maxPlayers: 2,
-    },
-    poster:
-      require("@site/static/img/home/games/thumbnails/SpookyCookie.mp4.png")
-        .default,
-    sources: [
-      {
-        src: require("@site/static/img/home/games/SpookyCookie.mp4.mp4")
-          .default,
-        type: "video/mp4",
-      },
-      {
-        src: require("@site/static/img/home/games/SpookyCookie.webmhd.webm")
-          .default,
-        type: "video/webm",
-      },
-    ],
-  },
-  {
-    game: {
-      title: "Rock Paper Shoot",
-      developers: [
-        {
-          name: "KIO Studios",
-          avatar:
-            "https://app.dusk.gg/avatar?base=12&hair=24&tilt=1&headgear=60&accessory=62&background=75&expression=17&size=90&isCropped=1&isBackgroundTransparent=0",
-        },
-      ],
-      minPlayers: 2,
-      maxPlayers: 2,
-    },
-    poster:
-      require("@site/static/img/home/games/thumbnails/RockPaperShoot.mp4.png")
-        .default,
-    sources: [
-      {
-        src: require("@site/static/img/home/games/RockPaperShoot.mp4.mp4")
-          .default,
-        type: "video/mp4",
-      },
-      {
-        src: require("@site/static/img/home/games/RockPaperShoot.webmhd.webm")
-          .default,
-        type: "video/webm",
-      },
-    ],
-  },
-  {
-    game: {
-      title: "Tavern Party",
-      developers: [
-        {
-          name: "wawasensei",
-          avatar:
-            "https://app.dusk.gg/avatar?base=10&hair=20&tilt=1&headgear=61&accessory=67&background=71&expression=16&size=90&isCropped=1&isBackgroundTransparent=0",
-        },
-      ],
-      minPlayers: 1,
-      maxPlayers: 4,
-    },
-    poster:
-      require("@site/static/img/home/games/thumbnails/TavernParty.mp4.png")
-        .default,
-    sources: [
-      {
-        src: require("@site/static/img/home/games/TavernParty.mp4.mp4").default,
-        type: "video/mp4",
-      },
-      {
-        src: require("@site/static/img/home/games/TavernParty.webmhd.webm")
-          .default,
-        type: "video/webm",
-      },
-    ],
-  },
-  {
-    game: {
-      title: "Pipeline Panic",
-      developers: [
-        {
-          name: "json",
-          avatar:
-            "https://app.dusk.gg/avatar?base=5&hair=43&tilt=1&headgear=59&accessory=67&background=95&expression=15&size=90&isCropped=1&isBackgroundTransparent=0",
-        },
-        {
-          name: "viturowski",
-          avatar:
-            "https://app.dusk.gg/avatar?base=12&hair=30&tilt=0&headgear=55&accessory=67&background=73&expression=15&size=90&isCropped=1&isBackgroundTransparent=0",
-        },
-      ],
-      minPlayers: 1,
-      maxPlayers: 4,
-    },
-    poster:
-      require("@site/static/img/home/games/thumbnails/PipelinePanic.mp4.png")
-        .default,
-    sources: [
-      {
-        src: require("@site/static/img/home/games/PipelinePanic.mp4.mp4")
-          .default,
-        type: "video/mp4",
-      },
-      {
-        src: require("@site/static/img/home/games/PipelinePanic.webmhd.webm")
-          .default,
-        type: "video/webm",
-      },
-    ],
-  },
-  {
-    game: {
-      title: "Neon Snake",
-      developers: [
-        {
-          name: "Helios1138",
-          avatar:
-            "https://app.dusk.gg/avatar?base=2&hair=44&tilt=0&headgear=56&accessory=66&background=72&expression=14&size=90&isCropped=1&isBackgroundTransparent=0",
-        },
-        {
-          name: "shanehelm",
-          avatar:
-            "https://app.dusk.gg/avatar?base=9&hair=49&tilt=1&headgear=76&accessory=66&background=97&expression=15&size=90&isCropped=1&isBackgroundTransparent=0",
-        },
-      ],
-      minPlayers: 1,
-      maxPlayers: 4,
-    },
-    poster: require("@site/static/img/home/games/thumbnails/NeonSnake.mp4.png")
-      .default,
-    sources: [
-      {
-        src: require("@site/static/img/home/games/NeonSnake.mp4.mp4").default,
-        type: "video/mp4",
-      },
-      {
-        src: require("@site/static/img/home/games/NeonSnake.webmhd.webm")
-          .default,
-        type: "video/webm",
-      },
-    ],
-  },
-]
-
 const scrollDuration = 600
 
-function Phone() {
+function Phone({ videos }) {
   const [cursor, setCursor] = useState(0)
 
   const currentVideoIndex = cursor % videos.length
@@ -316,7 +163,7 @@ export default function Home() {
       <div className={styles.home}>
         <div className={styles.content}>
           <div className={styles.hero1}>
-            <Phone />
+            <Phone videos={gameVideos.main} />
 
             <h2>build a multiplayer game played by millions</h2>
             <p>
@@ -419,15 +266,15 @@ export default function Home() {
 
             <ul>
               <li>
-                <Phone />
+                <Phone videos={gameVideos.main} />
                 <span className={styles.title}>Real-Time</span>
               </li>
               <li>
-                <Phone />
+                <Phone videos={gameVideos.main} />
                 <span className={styles.title}>Physics</span>
               </li>
               <li>
-                <Phone />
+                <Phone videos={gameVideos.main} />
                 <span className={styles.title}>Graphics</span>
               </li>
             </ul>
@@ -452,4 +299,8 @@ export default function Home() {
       </div>
     </Layout>
   )
+}
+
+Phone.propTypes = {
+  videos: PropTypes.array.isRequired,
 }

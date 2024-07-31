eec96c8fa56f05665e5a3fd5b67aa7e9f2ee0b14
Coke And Code <kevin@rune.ai>
2024-07-31 16:05:30 +0100

    docs: Update mail list subscription to be better (#501)
    * Update mail list subscription to be better
    
    * Fixes from review

--- docs/src/theme/BlogPostItem/Content/index.js
+++ docs/src/theme/BlogPostItem/Content/index.js
@@ -1,9 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import clsx from "clsx"
 import { blogPostContainerID } from "@docusaurus/utils-common"
 import { useBlogPost } from "@docusaurus/theme-common/internal"
 import MDXContent from "@theme/MDXContent"
+import styles from "./styles.module.css"
+
+async function subscribe(email) {
+  const response = await fetch("https://substackapi.com/api/subscribe", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email,
+      domain: "duskgg.substack.com",
+    }),
+  })
+  const json = await response.json()
+  if (json.errors?.length > 0) {
+    return json.errors[0].msg
+  }
+}
+
+const READY = 1
+const WORKING = 2
+const DONE = 3
+const ERROR = 4
+
 export default function BlogPostItemContent({ children, className }) {
+  const [email, setEmail] = useState("")
+  const [status, setStatus] = useState(READY)
+  const [error, setError] = useState(undefined)
+
   const { isBlogPostPage } = useBlogPost()
   return (
     <div
@@ -13,13 +39,84 @@ export default function BlogPostItemContent({ children, className }) {
       itemProp="articleBody"
     >
       <MDXContent>{children}</MDXContent>
-      <div style={{ width: "100%", textAlign: "center"}}>Subscribe to our mailing list for updates.</div>
-      <iframe
-        src="https://duskgg.substack.com/embed?cachebust=1"
-        width="100%"
-        height="150"
-        frameborder="0"
-      ></iframe>
+      <div style={{ width: "100%", textAlign: "center" }}>
+        Subscribe to our newsletter for more game dev blog posts
+      </div>
+
+      <div className={styles.emailWidget}>
+        {status === WORKING && (
+          <div className={styles.progressHolder}>
+            <div className={styles.progress}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        )}
+        {status === DONE && (
+          <div className={styles.thankyou}>
+            <div className={styles.thankyouHeading}>
+              Thanks for subscribing!
+            </div>
+            <div className={styles.thankyouBody}>
+              Keep an eye out for our next <b>Dusk Developer Update</b> from
+              Substack in your inbox.
+            </div>
+          </div>
+        )}
+        {(status === READY || status === ERROR) && (
+          <div>
+            <div className={styles.sideBarWrap}>
+              <div className={styles.emailWrapper}>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Type your email..."
+                  onChange={(e) => {
+                    setEmail(e.target.value)
+                  }}
+                  className={styles.emailInput}
+                />
+              </div>
+              <button
+                type="submit"
+                className={styles.subscribeButton}
+                tabindex="0"
+              >
+                <span
+                  onClick={async () => {
+                    setStatus(WORKING)
+                    setError(undefined)
+                    const error = await subscribe(email)
+                    if (error) {
+                      setStatus(ERROR)
+                      setError(error)
+                    } else {
+                      setStatus(DONE)
+                    }
+                  }}
+                >
+                  Subscribe
+                </span>
+              </button>
+            </div>
+            <div className={styles.subtext}>
+              <div className={styles.substack}>
+                We'll share your email with Substack
+                <div className={styles.tooltip}>
+                  &#9432;
+                  <span className={styles.tooltiptext}>
+                    Substack's embed form isn't very pretty, so we made our own.
+                    But we need to let you know we'll subscribe you on your
+                    behalf. Thanks in advance!
+                  </span>
+                </div>
+                {error && <div className={styles.error}>{error}</div>}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }

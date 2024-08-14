53728265797087859d47b146942cd2da3df14955
Daniel Mecke <daniel.mecke@cunningsoft.de>
2024-08-14 20:52:44 +0200

    feat: vite plugin allow imports from multiple files from one ignored dependency

--- packages/vite-plugin-dusk/src/plugins/detectExternalImports.ts
+++ packages/vite-plugin-dusk/src/plugins/detectExternalImports.ts
@@ -143,7 +143,8 @@ File: ${withOptionalExtension} was imported by:\n${importPath.join("\n")}
 
       const isInWhitelist =
         DEPENDENCY_WHITELIST.includes(name) ||
-        options.ignoredDependencies?.some(ignoredDependency => name.startsWith(ignoredDependency))
+        options.ignoredDependencies?.includes(name) ||
+        options.ignoredDependencies?.some(ignoredDependency => name.startsWith(ignoredDependency + "/"))
 
       if (skipAllowedDependencies && isInWhitelist) {
         return

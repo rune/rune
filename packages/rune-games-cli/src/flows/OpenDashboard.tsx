b2998be2f5f0338025ba3ab577c5a3628789989e
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2024-09-27 10:19:37 +0100

    refactor: show magic link (#553)
    * show magic link
    
    * swap order

--- packages/rune-games-cli/src/flows/OpenDashboard.tsx
+++ packages/rune-games-cli/src/flows/OpenDashboard.tsx
@@ -2,6 +2,7 @@ import { Box, Text } from "ink"
 import open from "open"
 import React, { useEffect, useState } from "react"
 
+import { Choose } from "../components/Choose.js"
 import { Step } from "../components/Step.js"
 import { useDashboardMagicLink } from "../gql/useMagicDashboardLink.js"
 import { formatApolloError } from "../lib/formatApolloError.js"
@@ -9,39 +10,64 @@ import { formatApolloError } from "../lib/formatApolloError.js"
 export function OpenDashboard() {
   const { createDashboardMagicLink, dashboardMagicLink, error } =
     useDashboardMagicLink()
-  const [status, setStatus] = useState<"waiting" | "opened" | "failedBrowser">(
-    "waiting"
-  )
+  const [status, setStatus] = useState<
+    "waiting" | "opened" | "failedBrowser" | "skipped"
+  >("waiting")
 
   useEffect(() => {
     createDashboardMagicLink({})
   }, [createDashboardMagicLink])
 
-  useEffect(() => {
-    if (dashboardMagicLink) {
-      open(dashboardMagicLink)
-        .then(() => {
-          setStatus("opened")
-        })
-        .catch(() => {
-          setStatus("failedBrowser")
-        })
-    }
-  }, [dashboardMagicLink])
-
   return (
     <Box flexDirection="column">
-      {!error && status === "waiting" && (
+      {!error && !dashboardMagicLink && (
         <Step status="waiting" label="Opening..." />
       )}
       {error && <Text color="red">{formatApolloError(error, {})}</Text>}
-      {status === "failedBrowser" && (
-        <Text color="yellow">
-          Failed to open your browser. Please open this link manually:{" "}
-          {dashboardMagicLink}
-        </Text>
+
+      {!error && dashboardMagicLink && (
+        <>
+          <Text>
+            Dashboard link: <Text color="green">{dashboardMagicLink}</Text>
+          </Text>
+
+          {status === "opened" ? (
+            <Step status="success" label="Dashboard opened" />
+          ) : status === "failedBrowser" ? (
+            <Step
+              status="error"
+              label="Failed to open your default browser. Please open the link manually"
+            />
+          ) : status === "skipped" ? (
+            <Step status="success" label="Done." />
+          ) : (
+            <Step
+              status={"userInput"}
+              label={"Open in default browser?"}
+              view={
+                <Choose
+                  options={["Yes", "No"]}
+                  onSubmit={(response) => {
+                    const shouldOpen = response === "Yes"
+
+                    if (shouldOpen) {
+                      open(dashboardMagicLink)
+                        .then(() => {
+                          setStatus("opened")
+                        })
+                        .catch(() => {
+                          setStatus("failedBrowser")
+                        })
+                    } else {
+                      setStatus("skipped")
+                    }
+                  }}
+                />
+              }
+            />
+          )}
+        </>
       )}
-      {status === "opened" && <Step status="success" label="Dashboard open!" />}
     </Box>
   )
 }

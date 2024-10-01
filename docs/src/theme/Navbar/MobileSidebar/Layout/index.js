135d7c0d3ed7ad9b20bfdcd473d0d718114dd36b
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2024-10-01 10:55:02 +0100

    fix: add sidebar back button (#560)
    add sidebar back button

--- docs/src/theme/Navbar/MobileSidebar/Layout/index.js
+++ docs/src/theme/Navbar/MobileSidebar/Layout/index.js
@@ -4,7 +4,8 @@ import { useNavbarSecondaryMenu } from "@docusaurus/theme-common/internal"
 import { Social } from "../../../../components/Social"
 
 export default function ({ header, primaryMenu, secondaryMenu }) {
-  const { shown: secondaryMenuShown } = useNavbarSecondaryMenu()
+  const { shown: secondaryMenuShown, hide } = useNavbarSecondaryMenu()
+
   return (
     <div className="navbar-sidebar">
       {header}
@@ -13,12 +14,36 @@ export default function ({ header, primaryMenu, secondaryMenu }) {
           "navbar-sidebar__items--show-secondary": secondaryMenuShown,
         })}
       >
-        <div className="navbar-sidebar__item menu">{primaryMenu}</div>
         <div className="navbar-sidebar__item menu">
+          {primaryMenu}
+
+          <div className="navbar-sidebar__footer">
+            <Social/>
+          </div>
+
+        </div>
+        <div className="navbar-sidebar__item menu">
+          <div className="menu__list-item" style={{
+            textAlign: 'center',
+            marginBottom: '20px',
+            paddingLeft: 0,
+            cursor: "pointer",
+            lineHeight: '100%',
+          }}>
+            <div className="menu__link" onClick={hide}>
+              <img
+                alt="back"
+                 style={{marginRight: '14px'}}
+                src={require("!!url-loader!!@site/static/img/sidebar-back.svg").default}
+              />
+              Back
+            </div>
+          </div>
+
           {secondaryMenu}
 
           <div className="navbar-sidebar__footer">
-            <Social />
+            <Social/>
           </div>
         </div>
       </div>
